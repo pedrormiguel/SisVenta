3cab995604086e43e0a1fdf969ab1702e03615f4
Pedro Ruiz <pedrormiguel@outlook.es>
2021-06-11 13:36:01 -0400

    Create AuditableEntity

---
+++ src/CORE/Domain/Common/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public class AuditableEntity
+    {
+        public AuditableEntity()
+        {
+
+        }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LasModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
